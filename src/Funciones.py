4ec786e02c502703cecb29a8acc4db15cc6400d0
Solrak <luiscarlosquesada@gmail.com>
2021-11-07 22:55:49 -0600

    Comedia

--- src/Funciones.py
+++ src/Funciones.py
@@ -3,21 +3,29 @@ import random as rd
 
 from pandas.tseries import frequencies
 from scipy import stats as sp
+import numpy as np
 
 #Generacion de un numero aleatorio basado en la distribución
 #De Poisson utilizando una funcion de densidad acumulativa inversa
-def inverseCDF(t, _lambda):
-    return -math.log(1 - t)/_lambda
+def inverseExpCDF(t, _lambda):
+    return -math.log(t)/_lambda
 
 
 
 #Generacion de tiempos entre llegadas en minutos de acuerdo a la cantidad de horas dada
 def exponential_generator(_lambda):
     r = rd.random()
-    inter_arrival_time = inverseCDF(r, _lambda)
+    inter_arrival_time = inverseExpCDF(r, _lambda)
     return inter_arrival_time
 
-
+def gamma_generator(alpha, _lambda):
+    r = rd.random()
+    for i in range(1, alpha):
+        rtmp = rd.random()
+        r *= rtmp
+        
+    variate = -1/(_lambda*alpha) * np.log(r)
+    return variate
 
 #Generacion de la lista completa de tiempos de llegada
 def interarrival_times(t, _lambda):
@@ -31,29 +39,24 @@ def interarrival_times(t, _lambda):
 
     return times
 
-
-#def gamma_random(alpha, lambd):
-#    rand_num = rd.random()
-#    rand_num = 
-#    return rand_num
-
-
 #Creacion de tiempo de servicio dependiendo del servidor a usar
 def service_time_fromID(id):
     if id == 0:
-        #r = np.random.gamma(7,1/3)
-        r = sp.gamma.pdf(7, 1/3)
-        
+        r = gamma_generator(7,1/3)
     elif id == 1:
-        #r = np.random.gamma(5,1/2)
-        r = sp.gamma.pdf(5,1/2)
-    
+        r = gamma_generator(5,1/2)
     elif id == 2:
-        #r = np.random.exponential(1/0.3)
         r = exponential_generator(1/0.3)
-
     elif id == 3:
-        #r = rd.uniform(4,9)
         r = rd.uniform(4,9)
 
     return r / 60
+
+
+#        l = []
+#        for i in range(0, 1000):
+#            x = service_time_fromID(1)
+#            print(x)
+#            l.append(x)
+#        import statistics
+#        print(statistics.mean(l))
--- src/Simulacion.py
+++ src/Simulacion.py
@@ -9,120 +9,97 @@ from Funciones import *
 
 
 # Variables importantes de la simulacion
+SIMULATION_RUNS = 1000
 SIMULATION_HOURS = 10
 WAIT_THRESHOLD = 6 / 60
 REJECTED_JOBS = 0
 
 
-# El array de servidores contiene el tiempo en que se realizo
-# la ejecucion final de cada uno, se explica más
-# adelante en el comentario de TEORIA
-servers_last_finished_times = [0, 0, 0, 0]
-last_arrival_time = 0
-interarrival_time = interarrival_times(SIMULATION_HOURS, 30)
-
-interarrival_times_list = []
-arrival_times_list = []
-wait_times_list = []
-service_times_list = []
-finish_times_list = []
-start_times_list = []
-system_times_list = []
+RUN_DATAFRAMES = []
 
 
-# Loop de simulacion
-for interarrival in interarrival_time:
+for i in range(SIMULATION_RUNS):
+    # El array de servidores contiene el tiempo en que se realizo
+    # la ejecucion final de cada uno, se explica más
+    # adelante en el comentario de TEORIA
+    servers_last_finished_times = [0, 0, 0, 0]
+    last_arrival_time = 0
+    interarrival_time = interarrival_times(SIMULATION_HOURS, 30)
 
-    # Calculo de los tiempos de llegada a partir de
-    # los tiempos entre arrivos
-    arrival_time = interarrival + last_arrival_time
-    last_arrival_time = arrival_time
+    interarrival_times_list = []
+    arrival_times_list = []
+    wait_times_list = []
+    service_times_list = []
+    finish_times_list = []
+    start_times_list = []
+    system_times_list = []
 
-    '''
-    Idea:
 
-    Iteracion 1:
-    Usar un array con tiempos finales,
-    asignar proceso a servidor con tiempo final mas bajo.
+    # Loop de simulacion
+    for interarrival in interarrival_time:
 
-    Iteracion 2:
-    Intentar implementar un threshold para 
-    poder sacar los jobs que tarden mas de 6 minutos
-    '''
-    # TEORIA:
-    # El servidor con tiempo mas pequeño
-    # Va a estar vacio o será el proximo
-    # En desocuparse
+        # Calculo de los tiempos de llegada a partir de
+        # los tiempos entre arrivos
+        arrival_time = interarrival + last_arrival_time
+        last_arrival_time = arrival_time
 
-    min_index = servers_last_finished_times.index(min(servers_last_finished_times))
+        '''
+        Idea:
 
-    #Revision de que el tiempo de espera no sea superior al threshold
-    if servers_last_finished_times[min_index] - arrival_time <= WAIT_THRESHOLD:
-        
-        # Entonces, esto deberá bastar para seleccionar el servidor
-        if arrival_time > servers_last_finished_times[min_index]:
-            init_time = arrival_time
+        Iteracion 1:
+        Usar un array con tiempos finales,
+        asignar proceso a servidor con tiempo final mas bajo.
+
+        Iteracion 2:
+        Intentar implementar un threshold para 
+        poder sacar los jobs que tarden mas de 6 minutos
+        '''
+        # TEORIA:
+        # El servidor con tiempo mas pequeño
+        # Va a estar vacio o será el proximo
+        # En desocuparse
+
+        min_index = servers_last_finished_times.index(min(servers_last_finished_times))
+
+        #Revision de que el tiempo de espera no sea superior al threshold
+        if servers_last_finished_times[min_index] - arrival_time <= WAIT_THRESHOLD:
+
+            # Entonces, esto deberá bastar para seleccionar el servidor
+            if arrival_time > servers_last_finished_times[min_index]:
+                init_time = arrival_time
+            else:
+                init_time = servers_last_finished_times[min_index]
+
+            # Ahora, es necesario elegir una funcion
+            # Que nos genere los valores para
+            # Tiempo de servicio de acuerdo al servidor
+            wait_time = init_time - arrival_time
+
+            service_time = service_time_fromID(min_index)
+
+            final_time = init_time + service_time
+
+            # Ajuste del tiempo final en el servidor
+            # que fue utilizado para la ejecucion actual
+            servers_last_finished_times[min_index] = final_time
+            # Ajuste al tiempo global - ERROR QUE ESTOY HACIENDO??
+            # ESTE ES EL TIEMPO EN EL SISTEMA NO EL GLOBAL
+            system_time = final_time - arrival_time
+
+            # Recoleccion de datos para estadisticas
+            interarrival_times_list.append(interarrival)
+            arrival_times_list.append(arrival_time)
+            wait_times_list.append(wait_time)
+            service_times_list.append(service_time)
+            finish_times_list.append(final_time)
+            start_times_list.append(init_time)
+            system_times_list.append(system_time)
+
         else:
-            init_time = servers_last_finished_times[min_index]
+            REJECTED_JOBS += 1
 
-        # Ahora, es necesario elegir una funcion
-        # Que nos genere los valores para
-        # Tiempo de servicio de acuerdo al servidor
-        wait_time = init_time - arrival_time
+    df = pd.DataFrame(data={
+        'Interarrival': interarrival_times_list, 'Arrival Time': arrival_times_list, 'Initial Service Time': start_times_list, 'Wait Time': wait_times_list, 'Service Time': service_times_list, 'Final Time': finish_times_list, 'System Time': system_times_list
+    })
 
-        service_time = service_time_fromID(min_index)
-
-        final_time = init_time + service_time
-
-        # Ajuste del tiempo final en el servidor
-        # que fue utilizado para la ejecucion actual
-        servers_last_finished_times[min_index] = final_time
-        # Ajuste al tiempo global - ERROR QUE ESTOY HACIENDO??
-        # ESTE ES EL TIEMPO EN EL SISTEMA NO EL GLOBAL
-        system_time = final_time - arrival_time
-
-        # Recoleccion de datos para estadisticas
-        interarrival_times_list.append(interarrival)
-        arrival_times_list.append(arrival_time)
-        wait_times_list.append(wait_time)
-        service_times_list.append(service_time)
-        finish_times_list.append(final_time)
-        start_times_list.append(init_time)
-        system_times_list.append(system_time)
-
-    else:
-        REJECTED_JOBS += 1
-
-df = pd.DataFrame(data={
-    'Interarrival': interarrival_times_list, 'Arrival time': arrival_times_list, 'Initial service time': start_times_list, 'Wait time': wait_times_list, 'Service time': service_times_list, 'Final time': finish_times_list, 'System time': system_times_list
-})
-
-
-print(df)
-
-print(sum(df['Interarrival']))
-print(statistics.mean(df['Interarrival']))
-print(REJECTED_JOBS)
-print(sum(df['Interarrival']))
-
-# Parametros a calcular
-# Expected response time
-# Expected waiting time for random job
-# Expected length of queue - jobs in service currently
-# Maximum waiting time 10 hrday
-# waiting_queue = []
-#print("Maximum waiting time:", max(waiting_queue))
-# Minimum waiting time 10 hrday
-#print("Minimum waiting time:", min(waiting_queue))
-# Probability that 1 server is available when job arrives
-# Probability that 2 servers_last_finished_times are available when job arrives
-# Expected number of jobs processed by each server
-# Expected idle time of each server during day
-# print("Expected idle times per server:")
-# print("Server 1 idle time:", statistics.mean(s1_idle_times))
-# print("Server 2 idle time:", statistics.mean(s2_idle_times))
-# print("Server 3 idle time:", statistics.mean(s3_idle_times))
-# print("Server 4 idle time:", statistics.mean(s4_idle_times))
-# Expected number of jobs remaining in system at 6:03pm
-
-# Expected percentaje of jobs lost
+    RUN_DATAFRAMES.append(df)
